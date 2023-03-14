fae1d4e1be57904883ed181f47350bff18d125e8
İlker DEMİR <ilkerdemir2834@outlook.com>
2023-03-14 14:16:55 +0300

    Events

---
+++ Events/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Products harddisk = new Products(40);
+            harddisk.ProductName = "Laptop";
+
+            Products gsm = new Products(40);
+            gsm.ProductName = "GSM";
+            gsm.StockControlEvent += Gsm_StockControlEvent;          
+
+            for(int i = 0; i < 10; i++)
+            {
+                harddisk.Sell(10);
+                gsm.Sell(10);
+                Console.ReadLine();              
+            }       
+
+            Console.ReadLine();
+
+        }
+
+        private static void Gsm_StockControlEvent()
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine("GSM about to finish!!!");
+        }
+    }
+}
---
+++ Reca Demo 2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reca_Demo_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager(new DatabaseLogger(),new FileLogger());
+            //customerManager.logger = new DatabaseLogger();
+            customerManager.Add();
+            Console.ReadLine();
+        }
+    }
+
+    class CustomerManager
+    {
+        //public ILogger logger { get; set; } // property injection
+
+        private ILogger[] _loggers;
+
+        public CustomerManager(params ILogger[] loggers)
+        {
+            _loggers = loggers;
+        }
+        public void Add()
+        {
+            foreach (var logger in _loggers)
+            {
+                logger.Log();
+            }
+            Console.WriteLine("Customer added!");            
+        }
+    }
+
+    class DatabaseLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to database.");
+        }
+    }
+
+    class FileLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to file.");
+        }
+    }
+
+    class SmsLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to sms.");
+        }
+    }
+
+    interface ILogger
+    {
+        void Log();
+    }
+}
